efb34fe24941a430a57947b478ba291f897bd07d
Daniel Silva <daniels.dev7@gmail.com>
2022-05-18 02:20:06 -0300

    Game quase finalizado

--- phaser3-project-template/src/Enemy.js
+++ phaser3-project-template/src/Enemy.js
@@ -1,4 +1,32 @@
-class Enemy extends Phaser.Physics.Arcade.Sprite {
+class Enemy extends Phaser.GameObjects.Sprite {
 
+    constructor(scene, x, y) {
+        super(scene, x, y, 'death').setScale(2).setOrigin(0.5, 0.5);
+        this.scene = scene;
+        this.scene.physics.world.enable(this);
+        this.scene.add.existing(this);
+    };
+
+    update() {
+        var dir = 0;
+        var vel = 2;
+        frames++;
+        if (frames < 100 && dir == 0) {
+            this.play('deathidle', true);
+        }
+        if (frames > 100) {
+            dir = 1;
+            this.play('deathidleL', true);
+        }
+        if (dir == 1 && frames > 200) {
+            dir = -1;
+            frames = 0;
+            this.play('deathidle', true);
+        }
+        this.x += vel * dir;
+
+    }
 
 }
+
+export default Enemy;
